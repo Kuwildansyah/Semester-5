97c6f450f083741b78b481003b9e76fbfa1849bf
ChocoChipz <59477151+Kuwildansyah@users.noreply.github.com>
2023-10-02 10:30:17 +0700

    commit all modul
    commit all modul

---
+++ Mobile/Modul_1/myusic/android/app/src/main/kotlin/com/example/myusic/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.myusic
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Mobile/Modul_1/myusic/lib/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Music Streaming App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Music Streaming App'),
+        backgroundColor: Colors.blue,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: "Search",
+                  hintText: "Search",
+                  prefixIcon: Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(25.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            // Recent Searches
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              height: 200.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 160.0,
+                    child: Card(
+                      color: Colors.blue,
+                      child: Text('Item $index'),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // Music Categories
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 20.0),
+              height: 200.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return SizedBox(
+                    width: 160.0,
+                    child: Card(
+                      color: Colors.red,
+                      child: Text('Item $index'),
+                    ),
+                  );
+                },
+              ),
+            ),
+            // Profile Button
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfilePage()),
+                );
+              },
+              child: const Text('Go to Profile'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Profile'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const CircleAvatar(
+              radius: 50,
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+            const Text(
+              'User Name',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'User Email',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+              width: 200,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade500,
+                  ),
+                  title: Text(
+                    '123-456-7890',
+                    style:
+                        TextStyle(color: Colors.teal.shade900, fontSize: 20.0),
+                  ),
+                )),
+            Card(
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade500,
+                  ),
+                  title: Text(
+                    'user.name@gmail.com',
+                    style:
+                        TextStyle(fontSize: 20.0, color: Colors.teal.shade900),
+                  ),
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
